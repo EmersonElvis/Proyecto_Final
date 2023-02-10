c9ed2fac19af3b63ec3756d86b0ce7ae2f0f94d0
juan <juan.quispem.27@unsch.edu.pe>
2023-02-10 13:35:52 -0500

    agregando determinante

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,44 @@
+import numpy as np
+#a=[[1,2,3,4,5],[1,4,12,31,5],[1,2,4,7,6],[1,9,8,7,0],[10,11,12,13,16]]
+#def calculatorDeterminantMatrix(matriz):
+#    determinante = np.linalg.det(matriz)
+#    return determinante
+#print(calculatorDeterminantMatrix(a)) 
+matriz=[]
+respuesta=[]
+def mat(n):
+    for i in range(n):
+        matriz.append([])
+        for j in range(n):
+            matriz[i].append(0)
+    return matriz
+def llenar(n):
+    matriz=mat(n)
+    for x in range(n):
+        for y in range(n):
+            matriz[x][y] = float(input(f"Valor de la fila {x+1} y columna {y+1}: "))
+        respuesta.append(float(input("Valor de la constante de la ecuacion "+str(x+1)+" = " )))
+
+def gjordan(n):
+    for z in range(n-1,0,-1):
+        for x in range (z):
+            if (matriz[z][z]!=0):
+                p =matriz[x][z]/matriz[z][z]
+                matriz[x][z]=matriz[x][z]-(matriz[z][z]* p)
+                respuesta[x]=respuesta[x]-(respuesta[z]*p)
+def gaussJordan(n):
+    for z in range(n-1,0,-1):
+        for x in range(z):
+            if (matriz[z][z] !=0 ):
+                p= matriz[x][z]/matriz[z][z]
+                matriz[x][z]=matriz[x][z]-(matriz[z][z]*p)
+                respuesta[x]=respuesta[x] - (respuesta[z]*p)
+def deter(n):
+    det=1
+    for x in range(n):
+        det =matriz[x][x]*det
+    print("\nEl valor de la determinante de la matriz es =",det)
+#Programa   
 def matrizTranspuesta():
 
     filas_MatrizA=  int(input("Ingrese la cantidad de filas de la matriz A => "))
@@ -69,6 +110,11 @@ while True:
             break
         elif Opcion=="D" or Opcion=="d" or Opcion=="DETERMINANTE" or Opcion=="determinante":
             #determinantematrices()
+            n=int(input("Tamaño  de la matriz: "))
+            llenar(n)
+            gjordan(n)
+            gaussJordan(n)
+            deter(n)
             break
         elif Opcion=="T" or Opcion=="t" or Opcion=="TRANSPUESTA" or Opcion=="transpuesta":
             #matrizTranspuesta()
