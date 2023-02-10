4828c803a1649933a983ceebfb2d4f312f78b0c9
Erik Jean Pierre VEGA MORENO <erik.vega.27@unsch.edu.pe>
2023-02-10 14:09:06 -0500

    SumaResta

--- Calculadora.py
+++ Calculadora.py
@@ -128,6 +128,82 @@ def inversaM(self):
             return np.linalg.inv(self.matriz)
         else:
             return ("La matriz a ingresar debe ser 'cuadrada'")
+
+def IngresarElementoMatriz(Fila,Columna):
+    Matriz = []
+    for i in range(Fila):
+        Fila_x = []
+        for j in range(Columna):
+            while True:
+                try:
+                    Elemento = float(input("Elemento (%d,%d): " %(i,j)))
+                except:
+                    continue
+                break
+            Fila_x.append(Elemento)
+        Matriz.append(Fila_x)
+    return Matriz
+
+def Pedir_ValidarDatoEntero(FilaColumna):
+    while True:
+        try:
+            fila_columna = int(input(f"{FilaColumna}:").lower().replace(" ",""))
+        except :
+            print("¡¡Digite bien!!")
+            continue
+        return fila_columna
+
+def sumaMatrices():
+    print("\n\tSuma de dos Matrices")
+    Fila = "Fila" ; Columna = "Columna"
+    print("\nPara la primera Matriz")
+    Filax = Pedir_ValidarDatoEntero(Fila) ; Columnax = Pedir_ValidarDatoEntero(Columna)
+    print("\nPara la segunda Matriz")
+    Filay = Pedir_ValidarDatoEntero(Fila) ; Columnay = Pedir_ValidarDatoEntero(Columna)
+    if Columnax == Columnay and Filax == Filay:
+        print("Llenando primera Matriz...")
+        MatrizA=IngresarElementoMatriz(Filax,Columnax) ; Matriz1 = np.array(MatrizA)
+        print("Llenando segunda Matriz...")
+        MatrizB=IngresarElementoMatriz(Filay,Columnay) ; Matriz2 = np.array(MatrizB)
+        Suma =[]
+        for i in range (Filax):
+            Suma.append( [0] * Columnax)
+            for j in range(Columnax):
+                Suma[i][j] += MatrizA[i][j] + MatrizB[i][j]
+
+        resultado = np.array(Suma)
+        print("\n\tMatriz A\n", Matriz1)
+        print("\n\tMatriz B\n", Matriz2)
+        print("\nLa suma de las matrices es:")
+        print(resultado)
+    else:
+        print("Recuerde la suma entre dos matrices debe ser de la misma dimension de filas y columnas")
+#RESTA DE MATRICES
+def restaMatrices():
+    print("\n\tSuma de dos Matrices")
+    Fila = "Fila" ; Columna = "Columna"
+    print("\nPara la primera Matriz")
+    Filax = Pedir_ValidarDatoEntero(Fila) ; Columnax = Pedir_ValidarDatoEntero(Columna)
+    print("\nPara la segunda Matriz")
+    Filay = Pedir_ValidarDatoEntero(Fila) ; Columnay = Pedir_ValidarDatoEntero(Columna)
+    if Columnax == Columnay and Filax == Filay:
+        print("Llenando primera Matriz...")
+        MatrizA=IngresarElementoMatriz(Filax,Columnax) ; Matriz1 = np.array(MatrizA)
+        print("Llenando segunda Matriz...")
+        MatrizB=IngresarElementoMatriz(Filay,Columnay) ; Matriz2 = np.array(MatrizB)
+        Resta =[]
+        for i in range (Filax):
+            Resta.append( [0] * Columnax)
+            for j in range(Columnax):
+                Resta[i][j] += MatrizA[i][j] - MatrizB[i][j]
+
+        resultado = np.array(Resta)
+        print("\n\tMatriz A\n", Matriz1)
+        print("\n\tMatriz B\n", Matriz2)
+        print("\nLa resta de las matrices es:")
+        print(resultado)
+    else:
+        print("Recuerde la resta entre dos matrices debe ser de la misma dimension de filas y columnas")
 #Menú General
 while True:
     #print("\tBIENVENIDO")
